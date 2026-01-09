f3d2703559f425bd8051546603d4b4503adbc6db
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2026-01-09 11:58:09 +0100

    Add files via upload

---
+++ Lernperiode 8/rust_denken/target/debug/deps/rust_denken-d81179c0ff8a3b54.d
@@ -0,0 +1,5 @@
+C:\Users\pioma\OneDrive - BBBaden\Lernatelier\Lernperiode 8\rust_denken\target\debug\deps\rust_denken-d81179c0ff8a3b54.d: src\main.rs
+
+C:\Users\pioma\OneDrive - BBBaden\Lernatelier\Lernperiode 8\rust_denken\target\debug\deps\librust_denken-d81179c0ff8a3b54.rmeta: src\main.rs
+
+src\main.rs:
---
+++ Lernperiode 8/rust_denken/target/debug/deps/rust_denken.d
@@ -0,0 +1,5 @@
+C:\Users\pioma\OneDrive - BBBaden\Lernatelier\Lernperiode 8\rust_denken\target\debug\deps\rust_denken.d: src\main.rs
+
+C:\Users\pioma\OneDrive - BBBaden\Lernatelier\Lernperiode 8\rust_denken\target\debug\deps\rust_denken.exe: src\main.rs
+
+src\main.rs:
